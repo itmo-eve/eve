01d86d0fde583e1f5e9e6c7de94a8b97d11d84b8
giggsoff <giggsoff@gmail.com>
2020-03-04 16:17:07 +0300

    fix annotations
    Signed-off-by: giggsoff <giggsoff@gmail.com>

--- pkg/pillar/cmd/eveadm/cmd/utils.go
+++ pkg/pillar/cmd/eveadm/cmd/utils.go
@@ -13,61 +13,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Envs string
 var Test bool
 var Run func (command *cobra.Command, timeout time.Duration, args []string, env string) (rerr, cerr error, stdout, stderr bytes.Buffer)
-
-func run_out (rerr error, cerr error, sout bytes.Buffer, serr bytes.Buffer) {
-	if rerr != nil {
-		fmt.Println("Command error: ", rerr.Error())
-		if cerr != nil {
-			fmt.Println("Command error: ",
-				cerr.Error())
-		}
-	}
-	fmt.Printf("Stdout:\n%s\n", sout.String())
-	fmt.Printf("Stderr:\n%s\n", serr.String())
-}
-
-// Run shell command with arguments and enviroment variables
-func rune(timeout time.Duration, args []string, env string) (rerr error, cerr error, stdout bytes.Buffer, stderr bytes.Buffer) {
-	var re error
-	var ce error
-	var sout bytes.Buffer
-	var serr bytes.Buffer
-
-	if len(args) > 0 {
-		Envs := strings.Split(env, " ")
-		cmd := args[0]
-		args = args[1:]
-		if timeout != 0 {
-			var cancel context.CancelFunc
-			cnt, cancel := context.WithTimeout(context.Background(),
-				timeout)
-			defer cancel()
-
-			cmd := exec.CommandContext(cnt, cmd, args...)
-			cmd.Stdout = &sout
-			cmd.Stderr = &serr
-			cmd.Env = append(os.Environ(), Envs...)
-
-			re = cmd.Run()
-			ce = cnt.Err()
-		} else {
-			cmd := exec.Command(cmd, args...)
-			cmd.Stdout = &sout
-			cmd.Stderr = &serr
-			cmd.Env = append(os.Environ(), Envs...)
-
-			re = cmd.Run()
-		}
-		if Verbose {
-			run_out(re, ce, sout, serr)
-		}
-	}
-	return re, ce, sout, serr
-}
-
 
 // Run shell command with arguments and enviroment variables
 func run(command *cobra.Command, timeout time.Duration, args []string, env string) (rerr error, cerr error, stdout bytes.Buffer, stderr bytes.Buffer) {
