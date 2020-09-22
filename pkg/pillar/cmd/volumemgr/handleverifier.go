6de115aa17c8f557e380646ed0b9c5782457c083
Petr Fedchenkov <giggsoff@gmail.com>
2020-09-22 11:11:46 +0300

    fix fatal inside verifier
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/pillar/cmd/volumemgr/handleverifier.go
+++ pkg/pillar/cmd/volumemgr/handleverifier.go
@@ -85,9 +85,10 @@ func MaybeRemoveVerifyImageConfig(ctx *volumemgrContext, imageSha string) {
 		return
 	}
 	if m.RefCount == 0 {
-		log.Fatalf("MaybeRemoveVerifyImageConfig: Attempting to reduce "+
+		log.Warnf("MaybeRemoveVerifyImageConfig: Attempting to reduce "+
 			"0 RefCount. Image Details - Name: %s, "+
 			"ImageSha256:%s", m.Name, m.ImageSha256)
+		return
 	}
 	m.RefCount -= 1
 	log.Infof("MaybeRemoveVerifyImageConfig: RefCount to %d for %s",
