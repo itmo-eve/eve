b98533fcd461ea83e272315a91e736060a51a208
Petr Fedchenkov <giggsoff@gmail.com>
2020-11-19 22:07:18 +0300

    resolveConfig notify on new datastore
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/pillar/cmd/downloader/datastoreconfig.go
+++ pkg/pillar/cmd/downloader/datastoreconfig.go
@@ -10,6 +10,7 @@ import (
 func handleDatastoreConfigCreate(ctxArg interface{}, key string,
 	configArg interface{}) {
 	handleDatastoreConfigImpl(ctxArg, key, configArg)
+	notifyResolveConfigByNewDatastoreConfig(ctxArg, key, configArg)
 }
 
 func handleDatastoreConfigModify(ctxArg interface{}, key string,
@@ -34,4 +35,23 @@ func handleDatastoreConfigDelete(ctxArg interface{}, key string,
 	cipherBlock := config.CipherBlockStatus
 	ctx.pubCipherBlockStatus.Unpublish(cipherBlock.Key())
 	log.Functionf("handleDatastoreConfigDelete for %s", key)
+}
+
+//notifyResolveConfigByNewDatastoreConfig fires modify handler for ResolveConfig
+//we need to call it in case of no DatastoreConfig found
+func notifyResolveConfigByNewDatastoreConfig(ctxArg interface{}, key string,
+	configArg interface{}) {
+
+	ctx := ctxArg.(*downloaderContext)
+	config := configArg.(types.DatastoreConfig)
+	log.Functionf("notifyResolveConfigByNewDatastoreConfig for %s", key)
+	resolveConfigs := ctx.subResolveConfig.GetAll()
+	for _, v := range resolveConfigs {
+		cfg := v.(types.ResolveConfig)
+		if cfg.DatastoreID == config.UUID {
+			//on this step we have ResolveConfig and we inside handleDatastoreConfigCreate
+			resHandler.modify(ctxArg, cfg.Key(), cfg, cfg)
+		}
+	}
+	log.Functionf("notifyResolveConfigByNewDatastoreConfig for %s, done", key)
 }
