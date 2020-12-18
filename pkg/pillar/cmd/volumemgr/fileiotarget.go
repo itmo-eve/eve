86879dfe4364a91466f7ab6ade48b243da3bcc31
Vitaliy Kuznetsov <ahil52_25@mail.ru>
2020-12-18 21:22:23 +0300

    fix

--- pkg/pillar/cmd/volumemgr/fileiotarget.go
+++ pkg/pillar/cmd/volumemgr/fileiotarget.go
@@ -27,37 +27,45 @@ func TargetCreate(status types.VolumeStatus) error {
 		//*/
 
 	var targetRoot = filepath.Join("/sys/kernel/config/target/core/fileio_0/", status.DisplayName)
-	if err := os.MkdirAll(targetRoot, os.ModePerm); err != nil {
+	log.Error("[targetRoot]>>>>" + targetRoot)
+	log.Error("[CWD]>>>>" + os.Getwd())
+	if err := os.MkdirAll(targetRoot, 0755); err != nil {
 		log.Error(fmt.Sprintf("Error create catalog in sysfs for target filio [%v]", err))
+		return err
 	}
 
 	var controlPath = filepath.Join(targetRoot, "control")
 	var data = fmt.Sprintf("fd_dev_name=%s,fd_dev_size=%d,fd_buffered_io=1", status.PathName(), status.CurrentSize)
 	if err := ioutil.WriteFile(controlPath, []byte(data), 0660); err != nil {
-		log.Error("Error set control")
+		log.Error(fmt.Sprintf("Error set control: %v", err))
+		return err
 	}
 
 	var bsPath = filepath.Join(targetRoot, "attrib", "block_size")
 	if err := ioutil.WriteFile(bsPath, []byte("4096"), 0660); err != nil {
-		log.Error("Error set bs")
+		log.Error(fmt.Sprintf("Error set bs: %v", err))
+		return err
 	}
 
 	var vpdUnitSerial = filepath.Join(targetRoot, "wwn", "vpd_unit_serial")
 	if err := ioutil.WriteFile(vpdUnitSerial, []byte(status.VolumeID.String()), 0660); err != nil {
-		log.Error("Error set UUID")
+		log.Error(fmt.Sprintf("Error set UUID: %v", err))
+		return err
 	}
 
 	var udevPath = filepath.Join(targetRoot, "udev_path")
 	if err := ioutil.WriteFile(udevPath, []byte(status.PathName()), 0660); err != nil {
-		log.Error("Error set udev")
+		log.Error(fmt.Sprintf("Error set udev", err))
+		return err
 	}
 
 	var enablePath = filepath.Join(targetRoot, "enable")
 	if err := ioutil.WriteFile(enablePath, []byte("1"), 0660); err != nil {
-		log.Error(err)
+		log.Error(fmt.Sprintf("Error set enable: %v", err))
+		return err
 	}
 
-	log.Error(fmt.Sprintf("Create target fileIO for [%v]:[%v] size=[%v] UUID:%s", status.DisplayName, status.PathName(), status.MaxVolSize, status.VolumeID))
+	log.Error(fmt.Sprintf("Created target fileIO for [%v]:[%v] size=[%v] UUID:%s", status.DisplayName, status.PathName(), status.MaxVolSize, status.VolumeID))
 
 	return nil
 }
